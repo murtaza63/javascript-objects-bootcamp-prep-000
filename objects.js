9279fc77d36f208b06f65c744bcf01b4c54afe91
breezy-constructor-6081 <murtaza63@gmail.com>
2019-10-23 06:08:15 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist= {"atif":"Tajdare"};
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist.artistName = songTitle;
+  return playlist;
+}
+/*function removeFromPlaylist(playlist,songTitle){
+  delete playlist.artistName;
+  return playlist;
+}*/
